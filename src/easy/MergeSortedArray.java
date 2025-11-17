80ea9c2107c8f0c19719104789365dd3a7fe5c82
BARATH PANDIAN K <132652556+bkbarath@users.noreply.github.com>
2025-11-17 21:55:07 +0530

    Merge Sorted Array Problem

---
+++ src/easy/MergeSortedArray.java
@@ -0,0 +1,39 @@
+package easy;
+
+import java.util.Arrays;
+
+public class MergeSortedArray {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+        merge2(nums1, m, nums2, n);
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
+        }
+        Arrays.sort(nums1);
+    }
+
+    public static void merge2(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        System.out.println(Arrays.toString(nums1));
+    }
+}
