6447a5e7763dbc7b64121d8a32919ee285306b60
BARATH PANDIAN K <132652556+bkbarath@users.noreply.github.com>
2025-11-18 21:02:50 +0530

    One and Two Bit Characters

---
+++ src/easy/OneAndTwoBitCharacters.java
@@ -0,0 +1,20 @@
+package easy;
+
+public class OneAndTwoBitCharacters {
+    public static void main(String[] args) {
+        int[] bits = {1,1,1,0};
+        System.out.println(isOneBitCharacter(bits));
+    }
+
+    public static boolean isOneBitCharacter(int[] bits) {
+
+        int n = bits.length;
+        int i = 0;
+
+        while (i < n - 1) {
+            i += bits[i] + 1;
+        }
+
+        return i == n - 1;
+    }
+}
