cfae6422363a61bfb36151589e0e8f0bba5a5eaa
BARATH PANDIAN K <132652556+bkbarath@users.noreply.github.com>
2025-11-27 22:00:54 +0530

    Search In Rotated Sorted Array

---
+++ src/medium/SearchInRotatedSortedArray.java
@@ -0,0 +1,41 @@
+package medium;
+
+public class SearchInRotatedSortedArray {
+    public static void main(String[] args) {
+        int[] nums = {4,5,6,7,0,1,2};  int target = 0;
+        System.out.println(search(nums, target));
+    }
+
+    public static int search(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // Left side is sorted
+            if (nums[left] <= nums[mid]) {
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            // Right side is sorted
+            else {
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
