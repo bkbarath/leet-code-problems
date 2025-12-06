6b3ced1e3688f1b48d694cdc0de0dd65305e97a2
bkbarath <barathcoder5@gmail.com>
2025-12-06 22:46:51 +0530

    Fixed the Three Sum Problem

---
+++ src/medium/ThreeSum.java
@@ -0,0 +1,54 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        System.out.println(threeSum(nums));
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = n - 1;
+            int target = -nums[i];
+
+            while (left < right) {
+
+                int sum = nums[left] + nums[right];
+
+                if (sum == target) {
+
+                    List<Integer> integers = new ArrayList<>(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    result.add(integers);
+
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
